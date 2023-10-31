c04d4956e134d371eed881cbaeec8b87933ab4ec
wang-zhou <wang.zhou@hp.com>
2023-10-31 15:53:54 +0800

    article crud

--- application/applet/applet.go
+++ application/applet/applet.go
@@ -7,9 +7,11 @@ import (
 	"github.com/wangzhou-ccc/beyond/application/applet/internal/config"
 	"github.com/wangzhou-ccc/beyond/application/applet/internal/handler"
 	"github.com/wangzhou-ccc/beyond/application/applet/internal/svc"
+	"github.com/wangzhou-ccc/beyond/pkg/xcode"
 
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 var configFile = flag.String("f", "etc/applet-api.yaml", "the config file")
@@ -25,6 +27,9 @@ func main() {
 
 	ctx := svc.NewServiceContext(c)
 	handler.RegisterHandlers(server, ctx)
+
+	// 自定义错误处理方法
+	httpx.SetErrorHandler(xcode.ErrHandler)
 
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
